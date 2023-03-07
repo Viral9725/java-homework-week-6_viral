e58687441c3fa2dbc9c86394729fbd375e660f77
viral9725 <viral.kakadiya@gmail.com>
2023-03-07 17:30:54 +0000

    Initial version

---
+++ src/Print_Programme/Program_1.java
@@ -0,0 +1,20 @@
+package Print_Programme;
+
+public class Program_1 {
+    public static void main(String[] args)
+    {
+        System.out.println("                Bio Data          ");
+        System.out.println("Name                     : Viral Kakadiya");
+        System.out.println("Date of Birth            : 20/01/1995");
+        System.out.println("Email Id                 : viral95@gmail.com");
+        System.out.println("Place of Birth           : Gujarat,India");
+        System.out.println("Address                  : 89,Stage Street,Harrow");
+        System.out.println("height                   : 5'9");
+        System.out.println("weight                   : 65 Kg");
+        System.out.println("Mobile Number            : +447958653525");
+        System.out.println("Religion                 : Hindu");
+        System.out.println("Religion                 : Patel");
+        System.out.println("Education                : Master in CS");
+        System.out.println("Hobby                    : Traveling,Reading");
+    }
+}
---
+++ src/Print_Programme/Program_8.java
@@ -0,0 +1,9 @@
+package Print_Programme;
+
+public class Program_8 {
+    public static void main(String[] args) {
+        int i = 50;
+        int j = 3;
+        System.out.println(i/j);
+    }
+}
